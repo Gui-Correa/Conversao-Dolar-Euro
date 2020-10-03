0b616a011baf31c784796c72f905ba94b9ea9ba0
Gui-Correa <71907354+Gui-Correa@users.noreply.github.com>
2020-10-02 22:24:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, d, e, conversao1, conversao2;
+            Console.WriteLine("Digite um valor em reais: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a cotação do dolar: ");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a cotação do Euro: ");
+            e = Convert.ToDouble(Console.ReadLine());
+
+            conversao1 = r * d;
+            conversao2 = r * e;
+
+            Console.WriteLine("O valor da converão entre reais e dolar é: " + conversao1);
+            Console.WriteLine("O valor da converão entre reais e euro é: " + conversao2);
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
